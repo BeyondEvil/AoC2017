99476ac6e0df90911b725837cf4a925153a154b7
Jim Brännlund <jim.brannlund@dporganizer.com>
2017-12-23 23:05:14 +0100

    last days prep

---
+++ 2017/day_25/solution.py
@@ -0,0 +1,15 @@
+
+def read_input():
+    with open('input.txt', 'r') as f:
+        return f.read().strip()
+
+
+def run_it(seq):
+
+    print('Part 1: ', 0)
+    print('Part 2: ', 0)
+
+
+if __name__ == '__main__':
+
+    run_it(read_input())  # ,
