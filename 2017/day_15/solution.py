8fcb4db1b9c8dae49e0e46fe6cdea9701d0f1dbc
Jim Brännlund <jim.brannlund@dporganizer.com>
2017-12-23 23:25:00 +0100

    day 15 small optimization

--- 2017/day_15/solution.py
+++ 2017/day_15/solution.py
@@ -2,7 +2,7 @@
 
 def get_value(start, factor, product, condition):
     while True:
-        _, start = divmod(start * factor, product)
+        start = (start * factor) % product
         if start % condition == 0:
             yield start
 
@@ -13,15 +13,13 @@ def run_it(start_a, start_b):
     factor_b = 48271
     product = 2147483647
     matches_a = 0
-    mask = (1 << 16) - 1
+    mask = (1 << 16) - 1  # 65535
     a = start_a
     b = start_b
     for iteration in range(iterations):
-        _, a = divmod(a * factor_a, product)
-        _, b = divmod(b * factor_b, product)
-        res_a = '{:08b}'.format(a & mask)
-        res_b = '{:08b}'.format(b & mask)
-        if res_a == res_b:
+        a = (a * factor_a) % product
+        b = (b * factor_b) % product
+        if a & mask == b & mask:
             matches_a += 1
 
     iterations = int(5e6)
@@ -31,9 +29,7 @@ def run_it(start_a, start_b):
     for _ in range(iterations):
         a = next(get_value(a, factor_a, product, 4))
         b = next(get_value(b, factor_b, product, 8))
-        res_a = '{:08b}'.format(a & mask)
-        res_b = '{:08b}'.format(b & mask)
-        if res_a == res_b:
+        if a & mask == b & mask:
             matches_b += 1
 
     print('Part 1: ', matches_a)
