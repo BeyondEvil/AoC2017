6e0f5f8410a0ad44d56b1923807e7163117b2835
Jim Brännlund <jim.brannlund@dporganizer.com>
2017-12-26 00:56:43 +0100

    day 25

--- 2017/day_25/solution.py
+++ 2017/day_25/solution.py
@@ -1,35 +1,31 @@
+from collections import defaultdict
 
-states = {('A', 0): {'value': 1, 'move': 'right', 'next': 'B'},
-          ('A', 1): {'value': 0, 'move': 'right', 'next': 'C'},
-          ('B', 0): {'value': 0, 'move': 'left', 'next': 'A'},
-          ('B', 1): {'value': 0, 'move': 'right', 'next': 'D'},
-          ('C', 0): {'value': 1, 'move': 'right', 'next': 'D'},
-          ('C', 1): {'value': 1, 'move': 'right', 'next': 'A'},
-          ('D', 0): {'value': 1, 'move': 'left', 'next': 'E'},
-          ('D', 1): {'value': 0, 'move': 'left', 'next': 'D'},
-          ('E', 0): {'value': 1, 'move': 'right', 'next': 'F'},
-          ('E', 1): {'value': 1, 'move': 'left', 'next': 'B'},
-          ('F', 0): {'value': 1, 'move': 'right', 'next': 'A'},
-          ('F', 1): {'value': 1, 'move': 'right', 'next': 'E'}}
-
-
-test_states = {('A', 0): {'value': 1, 'move': 'right', 'next': 'B'},
-               ('A', 1): {'value': 0, 'move': 'left', 'next': 'B'},
-               ('B', 0): {'value': 1, 'move': 'left', 'next': 'A'},
-               ('B', 1): {'value': 1, 'move': 'right', 'next': 'A'}}
+real_states = {('A', 0): (1, 1, 'B'),
+               ('A', 1): (0, 1, 'C'),
+               ('B', 0): (0, -1, 'A'),
+               ('B', 1): (0, 1, 'D'),
+               ('C', 0): (1, 1, 'D'),
+               ('C', 1): (1, 1, 'A'),
+               ('D', 0): (1, -1, 'E'),
+               ('D', 1): (0, -1, 'D'),
+               ('E', 0): (1, 1, 'F'),
+               ('E', 1): (1, -1, 'B'),
+               ('F', 0): (1, 1, 'A'),
+               ('F', 1): (1, 1, 'E')}
 
 
-def read_input():
-    with open('input.txt', 'r') as f:
-        return f.read().strip()
+def run_it(steps, states):
+    tape = defaultdict(int)
+    next_state = 'A'
+    index = 0
+    for step in range(steps):
+        value, direction, next_state = states.get((next_state, tape.get(index, 0)))
+        tape[index] = value
+        index += direction
 
-
-def run_it(seq):
-
-    print('Part 1: ', 0)
-    print('Part 2: ', 0)
+    print('Part 1: ', sum(tape.values()))
 
 
 if __name__ == '__main__':
 
-    run_it(read_input())  # ,
+    run_it(12399302, real_states)  # 2794,
