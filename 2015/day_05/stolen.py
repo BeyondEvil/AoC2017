e48446a0b5c27cec778b62f1c8aa85a1892831f5
Jim Brännlund <jim.brannlund@dporganizer.com>
2017-12-31 13:55:35 +0100

    styling

--- 2015/day_05/stolen.py
+++ 2015/day_05/stolen.py
@@ -22,15 +22,22 @@ for line in DATA:
     isnice1 = True
     isnice2 = True
 
-    if not re.search(hasvowels, line): isnice1 = False
-    if not re.search(hasapair, line): isnice1 = False
-    if re.search(hasgroups, line): isnice1 = False
+    if not re.search(hasvowels, line):
+        isnice1 = False
+    if not re.search(hasapair, line):
+        isnice1 = False
+    if re.search(hasgroups, line):
+        isnice1 = False
 
-    if not re.search(hasdpair, line): isnice2 = False
-    if not re.search(haspairsep, line): isnice2 = False
+    if not re.search(hasdpair, line):
+        isnice2 = False
+    if not re.search(haspairsep, line):
+        isnice2 = False
 
-    if isnice1: nice1 += 1
-    if isnice2: nice2 += 1
+    if isnice1:
+        nice1 += 1
+    if isnice2:
+        nice2 += 1
 
 print ("Number of part 1 nice lines: "+str(nice1))
 print ("Number of part 2 nice lines: "+str(nice2))
