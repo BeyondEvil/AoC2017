a223dbcaf486f458d7fb10205d0d4caafa64861d
Jim Brännlund <jim.brannlund@dporganizer.com>
2018-01-03 00:30:53 +0100

    2015 day 8 complete

--- 2015/day_08/solution.py
+++ 2015/day_08/solution.py
@@ -6,23 +6,36 @@ def read_input():
         return f.read().strip()
 
 
-def run_it(seq):
-
+def in_memory(text):
     ascii_pattern = r'\\x\w{2}'
-    total = 0
+    text = text.replace('\\\\', 'A') \
+        .replace('\\"', 'A') \
+        .replace('"', '')
+    return re.sub(ascii_pattern, 'A', text)
+
+
+def encoded(text):
+    text = text.replace('\\', '\\\\') \
+        .replace('"', '\\"')
+    return '"' + text + '"'
+
+
+def run_it(seq):
+    memorized = 0
+    encoderized = 0
     for literal in seq.splitlines():
         code_size = len(literal)
-        literal = literal.replace('\\\\', 'A')\
-            .replace('\\"', 'A')\
-            .replace('"', '')
-        literal = re.sub(ascii_pattern, 'A', literal)
-        string_size = len(literal)
-        total += code_size - string_size
 
-    print('Part 1: ', total)
-    print('Part 2: ', 0)
+        in_mem = in_memory(literal)
+        memorized += code_size - len(in_mem)
+
+        enc = encoded(literal)
+        encoderized += len(enc) - code_size
+
+    print('Part 1: ', memorized)
+    print('Part 2: ', encoderized)
 
 
 if __name__ == '__main__':
 
-    run_it(read_input())  # 1333,
+    run_it(read_input())  # 1333, 2046
